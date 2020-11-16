066b46f85a512be215f3380ee02dd5e6e10c9a5d
Daniel Dabrowski <agrcom@yahoo.pl>
2020-11-16 21:16:54 +0100

    spring retry example - added customException

---
+++ src/main/java/com/agrcom/springretry/exception/RepositoryException.java
@@ -0,0 +1,8 @@
+package com.agrcom.springretry.exception;
+
+public class RepositoryException extends Exception {
+
+    public RepositoryException(String message) {
+        super(message);
+    }
+}
